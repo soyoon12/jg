eeb71d5e04c30a7d5ca18b3b3589151fc9fbd90a
soyoon <soyoonp@naver.com>
2021-03-23 21:43:37 +0900

    tnwjd

--- class_7/js/jquery_ui.js
+++ class_7/js/jquery_ui.js
@@ -1,44 +1,60 @@
-$(function(){
-    $(".input_area input[type='text']").keypress(function(e){
-        if(e.keyCode == 13  && $(this).val().length > 0){//엔터를 누를경우와 입력하고 있는 input에 값이 있을때 실행하는 조건문
-            var _val = $(this).val();//입력된 input의 내용을 담는 변수
-            var _class =$(this).attr("class");//입력된 input의 클래스명을 담는 변수
-            var _time; //_time에 시간관련 변수를 합하여 저장
+$(function () {
+    //엔터 눌렀을때 발생하는 이벤트로 동적으로 만들자
+    $(".input_area > input[type='text']").keypress(function (event) {
+        // event는 keypress 함수의 인자값으로 keypress가 가지고 있는 정보들을 담기 위해 준것??
+        // console.log(event); 를 쳐보면 여러 속성 옵션값이 나타난다.
 
-            //현재 시간 구하기
-            var _data = new Date();//pc의 전체 시간 정보
-            var _hh = _data.getHours();//시간정보중 시간(hour)만 저장
-            var _mm = _data.getMinutes();//시간정보중 분(minutes)만 저장
+        // console.log($(this).val().length);
+        // 엔터키 코드 십삼일 경우와 인풋에 값이 들어갈 경우
+        if (event.keyCode == 13 && $(this).val().length != 0) {
+            //여기서 this는 인풋
+            var _val = $(this).val();//입력중인 값을 담는 변수
+            var _time; // 입력되는 순간의 시간을 담는 변수
+            var _class = $(this).attr("class");
+
+            //현재 시간 구하기 객체 호출
+            var _date = new Date();//date 객체는 pc의 전체 시간정보를 담고 있다
+            var _hh = _date.getHours();//date 객체에서 시간을 구함
+            var _mm = _date.getMinutes();//date 객체에서 분을 구함
+            // console.log(_date.getHours(),_date.getMinutes());
+
+            // 오전 오후 구분하기 위한 변수
             var _apm = "오전";
-            if(_hh > 12){
+            // 시간이 12이상인 경우
+            if (_hh > 12) {
                 _apm = "오후";
-                _hh = _hh - 12;//기존 시간변수(_hh)에서 12를 빼고 다시 _hh에 저장
+                _hh -= 12; //_hh = _hh - 12; 24시간에서 12시간 표현으로 바꾸어 쥼
             }
-            if(_hh < 10) _hh = "0" + _hh; //시간이 한자리일경우(0~9) 앞에 문자(0)을 추가
-            if(_mm < 10) _mm = "0" + _mm; //분이 한자리일경우(0~9) 앞에 문자(0)을 추가
-            _time = _apm+":"+_hh+":"+_mm; //_time에 시간관련 변수를 합하여 저장
-            //현재 시간 구하기 끝
 
-            //말풍선 태그를 추가하는 append
-            $(".chat_area").append('<div class="item '+_class+'"><div class="box"><p class="msg">'+_val+'</p><span class="time">'+_time+'</span></div></div>');
-            
-            setTimeout(function(){
-                $(".chat_area .item").last().addClass("on");//트렌지션 효과를 쓰기위해 0.01초딜레이타임 이후 on클래스를 추가해줌
-                
-                //내용추가후 스크롤을 맨 밑으로 내림
-                var _itemH = $(".chat_area .item").height() + 15; //아이템의 높이 체크
-                var _itemC = $(".chat_area .item").length; //아이템의 갯수 체크
-                var _itemTotal = _itemH * _itemC - 15; //위의 높이와 갯수를 계산해 총높이값 저장
-                
-                //채팅창이 맨밑으로 갈수 있게 하는 스크롤 에니메이션
-                $(".chat_area").stop().animate({
-                    scrollTop:_itemTotal
-                },500);
-            },10)
+            _time = _apm + " " + _hh + ":" + _mm;
+            console.log(_time);
 
+            //말풍선 태그를 append로 동적추가
+            $(".chat_area").append('<div class="item ' + _class + '"> <div class="box"><p class="msg">' + _val + '</p><span class="time">' + _time + '</span></div></div>')
+            //0.01초뒤에 해당 코드 시간차 발생하여 실행 
+            setTimeout(function () {
+                $(".chat_area .item").addClass("on");//css 애니메이션 발생시키는 트리거
+            }, 10)
+            //입력후 입력된 인풋의 값을 지워줌(초기화)
+            $(this).val("");
 
-            
-            $(this).val("");//input의 입력된 내용을 삭제
-        };
+            //채팅창 맨밑으로 갈수있게 하는 스크롤 이벤트
+            // //먼저 말풍선 개수 알기
+            // var _itemL =  $(".chat_area .item").length;
+            //말풍선의 높이를 알아야 한다.-> 더해줄 변수
+            var _itemH = 0;
+            //item의 개수만큼 반복시켜준다는 each문
+            $(".chat_area .item").each(function (idx) {
+                // console.log(idx);
+                //this(말풍선) 길이를 _item에 누적 추가+ margin-top값
+                _itemH +=  $(this).height() +15 ;//_itemH = _itemH + $(this).height() +15 ; 15는 margin 값
+            });
+            console.log(_itemH);
+            //채팅창 영역에 스크롤 에니메이션 이벤트 발생시킴 모든 동작을 스탑시키고 에니메이션 최종만 실행
+            $(".chat_area").stop().animate({
+                scrollTop:_itemH 
+            })
+        }
+
     });
-})
+});
